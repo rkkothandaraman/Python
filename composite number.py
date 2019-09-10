a81115e9e5dadd0b331052ae5a8bb07388c12e8b
rkkothandaraman <52990672+rkkothandaraman@users.noreply.github.com>
2019-09-11 00:06:11 +0530

    Add files via upload

--- composite number.py
+++ composite number.py
@@ -10,13 +10,11 @@
 #-------------------------------------------------------------------------------
 count=0
 comp=int(input())
-if(comp<=0):
-    print("Please enter a valid number !")
-else:
+if(comp>0):
     for i in range(1,comp):
          if(comp%i==0):
                 count=count+1
     if(count>2):
-         print("Yes")
+         print("yes")
     else:
-        print("No")
+        print("no")
