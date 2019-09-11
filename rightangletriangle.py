90882fd71190602ef594e301d5349c14a5277318
rkkothandaraman <52990672+rkkothandaraman@users.noreply.github.com>
2019-09-11 16:03:06 +0530

    Update rightangletriangle.py

--- rightangletriangle.py
+++ rightangletriangle.py
@@ -1,11 +1,11 @@
 x,y,z=map(int,input().split())
 x,y,z=sorted([x,y,z])
 if(x>=100000 or y>=100000 or z>=100000):
-    print("No")
+    print("no")
 elif(x and y and z):
     if((x+y>=z) and (x!=y!=z) and (x**2+y**2==z**2)):
-        print("Yes")
+        print("yes")
     else:
-        print("No")
+        print("no")
 else:
-    print("No")
+    print("no")
