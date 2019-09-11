51d6bfe81464f927fae04ba775d1061b92019b25
rkkothandaraman <52990672+rkkothandaraman@users.noreply.github.com>
2019-09-11 09:32:43 +0530

    Update scalenetriangle.py

--- scalenetriangle.py
+++ scalenetriangle.py
@@ -1,8 +1,5 @@
 x,y,z=map(int,input().split())
-if(x and y and z):
+if((x and y and z)and(x<=100000 and y<=100000 and z<=100000)):
     if((x+y>=z or y+z>=x or x+z>=y) and (x!=y!=z)):
         print("Yes")
-    else:
-        print("No")
-else:
-    print("No")
+    
